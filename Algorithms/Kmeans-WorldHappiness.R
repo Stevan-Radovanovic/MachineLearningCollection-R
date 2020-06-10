d1105afb5faba16071e1e7ad71951f4b496fa24d
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-06-10 18:29:58 +0200

    Commit1 - KMeans v0.1

--- Algorithms/Kmeans-WorldHappiness.R
+++ Algorithms/Kmeans-WorldHappiness.R
@@ -1,2 +1,29 @@
 data <- read.csv("world-happiness-report-2017.csv")
 str(data)
+summary(data)
+
+#Uklanjanje NA vrednosti
+which(is.na(data))
+
+#Uklanjanje NA vrednosti Health.Life.Expectancy
+shapiro.test(data$Health.Life.Expectancy)
+#Nema normalnu distribuciju
+data$Health.Life.Expectancy[is.na(data$Health.Life.Expectancy)] <- median(data$Health.Life.Expectancy,na.rm=T)
+
+#Uklanjanje NA vrednosti Generosity
+shapiro.test(data$Generosity)
+#Nema normalnu distribuciju
+data$Generosity[is.na(data$Generosity)] <- median(data$Generosity,na.rm = T)
+
+#Ponovna provera NA vrednosti
+which(is.na(data))
+
+#Uklanjanje autlajera
+
+#Standardizacija
+
+#Odredjivanje optimalnog K
+
+#Pravljenje modela
+
+#Komentarisanje modela
